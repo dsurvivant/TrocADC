1c2a5d37a814a1b32086deb6bbff8e16ee3e082c
dsurvivant <jmtentelier@gmail.com>
2021-05-07 13:10:57 +0200

    ajax correction bdd
    correction chemin bdd

--- public/js/ajax/findresidences.php
+++ public/js/ajax/findresidences.php
@@ -6,23 +6,10 @@
  *
  * Retourne le select html avec les résidences
  */
+require("../../../model/config.php");
 require("../../../classes/Residence.class.php");
 require("../../../classes/ResidenceManager.class.php");
 
-try{
-		$bdd = new PDO('mysql:host=localhost;dbname=trocadc','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND=> 'SET NAMES UTF8'));	
-		/**
-		$host='hallidz62.mysql.db';
-		$db='hallidz62';
-		$user='hallidz62';
-		$pw='Ayddacnathof5';
-		$bdd = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pw, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));**/
-	}
-	catch(PDOException $e)
-	{
-		echo 'Base de donnees en vacances';
-		exit();
-	}
 //
 if (isset($_POST['idup'])) {$idup = $_POST['idup'];}
 else { $idup ='';}
