570cf5538b27a922c9650d52eef4d0d1f2ad88f3
dsurvivant <jmtentelier@gmail.com>
2021-07-14 18:32:06 +0200

    suppression message limitation 171

--- view/public/messages/view_message_confirmation_activation.php
+++ view/public/messages/view_message_confirmation_activation.php
@@ -8,11 +8,6 @@
 			Ce compte sera validé dans les 24 heures. Un message de confirmation d'activation te sera adressé (sms ou mail). <br>
 		</p>
 
-		<p class="col-12 border bg-secondary text-center text-white p-3">
-			RAPPEL: Seuls les agents du 171 PE sont autorisés à cette application pour le moment. <br>
-			Cette appli est actuellement en test par ces agents.
-		</p>
-
 		<div class="text-center col-12"><a class="btn-secondary text-white p-2" href="index.php">Retour</button></a>
 	</div>
 </div>
