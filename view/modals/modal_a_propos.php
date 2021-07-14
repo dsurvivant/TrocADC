2f33c4ea4f5d07ad1693860459063cf52257f540
dsurvivant <jmtentelier@gmail.com>
2021-07-14 18:19:43 +0200

    mis à jour

--- view/modals/modal_a_propos.php
+++ view/modals/modal_a_propos.php
@@ -8,16 +8,6 @@
 
 	        <div class="modal-body">
 	        	Actuellement en cours de développement. <br>
-	        	trocADC@yahoo.com - V1.0
-	        	<br><br>
-
-	        	<span class="text-monospace">Dernières modifs:</span><br>
-	        		<div class="lead">
-	        			- Possibilité d'ajouter une journée FAC.<br>
-		        		- Liste des 10 dernières journées proposées. <br>
-		        		- Dans les paramètres: possibilité de choisir d'afficher ou pas son nom prénom et mail.
-		        		le téléphone reste indispensable
-		        	</div>
 	        </div>
 	    </div>
 	</div>
